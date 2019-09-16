b181eb77f73b41deeec8a28e2a1517d174f34af5
alla-d <aviator.nr1@gmail.com>
2019-09-16 22:36:13 +0200

    Add new classes WorkingWithElementsList and RadioButtonsAndCheckBoxes

---
+++ src/test/java/RadioButtonsAndCheckBoxes.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class RadioButtonsAndCheckBoxes {
+
+    WebDriver driver;
+    String baseUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://learn.letskodeit.com/p/practice";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void radioAndCheckbox() throws Exception {
+        driver.get(baseUrl);
+        WebElement benzRadio = driver.findElement(By.id("benzradio"));
+        benzRadio.click(); // click on radiobutton, where type= 'radio'
+        WebElement hondaRadio =driver.findElement(By.id("hondaradio"));
+        hondaRadio.click();
+
+
+        driver.findElement(By.id("hondacheck")).click(); //click on checkbox, where type= 'checkbox'
+        driver.findElement(By.id("benzcheck")).click();
+        Thread.sleep(2000);
+
+        System.out.println("Benz Radio is selected? " + benzRadio.isSelected()); // return true or false
+        System.out.println("Honda Radio is selected? " + hondaRadio.isSelected());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
---
+++ src/test/java/WorkingWithElementsList.java
@@ -0,0 +1,48 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class WorkingWithElementsList {
+
+    WebDriver driver;
+    String baseUrl;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://learn.letskodeit.com/p/practice";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public void testListOfElements() throws Exception {
+        boolean isChecked = false;
+        List<WebElement> radioButtons = driver.findElements(
+                By.xpath("//input[contains(@type, 'radio') and contains(@name, 'cars')]"));
+        int size = radioButtons.size();
+        System.out.println("Size of the list: " + size);
+        for (int i =0; i<size; i++){
+            isChecked = radioButtons.get(i).isSelected();
+
+            if (!isChecked){
+                radioButtons.get(i).click();
+                Thread.sleep(200);
+            }
+
+        }
+        }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
