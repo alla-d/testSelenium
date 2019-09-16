c1136e65eb6d8f63bea2094f9c4d7c9d5e3c2051
alla-d <45107049+alla-d@users.noreply.github.com>
2019-09-16 21:25:58 +0200

    Delete UniqueNameWith.groovy

--- src/test/java/UniqueNameWith.groovy
+++
@@ -1,6 +0,0 @@
-def date = new Date().format("MMdd")
-def time = new Date().format("HHmmss")
-
-name = "Name " + date + "_" + time
-
-// testRunner.getTestCase().setPropertyValue("name", name); // Useful for SoapUI properties
